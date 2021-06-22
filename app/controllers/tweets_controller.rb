cf7a350f19c5bfcb2be0a5cdd7dfac4887737c33
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-06-23 08:27:45 +0900

    showアクション定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -25,6 +25,10 @@ class TweetsController < ApplicationController
     tweet = Tweet.find(params[:id])
     tweet.update(tweet_params)
   end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
   
   private
   def tweet_params
