46c52350084bb90ce6da1a66c345d74ec7239df0
nina2728 <80686366+nina2728@users.noreply.github.com>
2021-06-22 20:29:28 +0900

    バリデーション編集

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,9 @@
 class Tweet < ApplicationRecord
   has_one_attached :image
-  validates :text, presence: true
+
+  validates :text, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.was_attached?
+  end
 end
